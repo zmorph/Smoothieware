4c283a86147535388eca59b748501aba5206ea4c
kypp <kyp108@gmail.com>
2014-03-04 19:15:00 +0000

    USBMessageStream modified to resemble USBSerial

--- src/libs/USBDevice/USBHID/USBMessageStream.cpp
+++ src/libs/USBDevice/USBHID/USBMessageStream.cpp
@@ -7,70 +7,210 @@
 #include "libs/Kernel.h"
 #include "libs/SerialMessage.h"
 
-USBMessageStream::USBMessageStream(USB *u) : USBHID(u, 64,64){}
+USBMessageStream::USBMessageStream(USB *u) : USBHID(u, 64,64),
+	rxbuf(256 + 8), 
+	txbuf(128 + 8),
+	nl_in_rx(0),
+	flush_to_nl(false)
+{}
 
 
 void USBMessageStream::on_module_loaded()
 {
     this->register_for_event(ON_MAIN_LOOP);
+    THEKERNEL->streams->append_stream(this);
+}
+
+uint8_t USBMessageStream::available()
+{
+    return rxbuf.available();
 }
 
 void USBMessageStream::on_main_loop(void *argument)
 {
-	// execute all pending messages
-	/*HID_REPORT recv_report;
-	//THEKERNEL->streams->printf("befor puts");
-	//puts("sabaka");
-	//THEKERNEL->streams->printf("after puts");
-	while (readNB(&recv_report)) 
-	{
-		THEKERNEL->streams->printf("I have recieved an HID report!\n");
-
-        struct SerialMessage message;
-        // ZTODO the following conversion is dangerous - unsigned to signed
-        message.message = string((const char *)recv_report.data);
-        if (message.message.find("sendme") != string::npos)
-        	puts("something");
-        message.stream = this;
-        THEKERNEL->streams->printf("message: %s", message.message.c_str());
-        THEKERNEL->call_event(ON_CONSOLE_LINE_RECEIVED, &message);
-	}*/
+    if (nl_in_rx)
+    {
+        string received;
+        while (available())
+        {
+            char c = _getc();
+            if( c == '\n' || c == '\r')
+            {
+                struct SerialMessage message;
+                message.message = received;
+                message.stream = this;
+                iprintf("USBMessageStream Received: %s\n", message.message.c_str());
+                THEKERNEL->call_event(ON_CONSOLE_LINE_RECEIVED, &message );
+                return;
+            }
+            else
+            {
+                received += c;
+            }
+        }
+    }
 }
 
 int USBMessageStream::puts(const char *str)
 {
-	// ZTODO check for strings longer then 64 bytes
-
-	int length = strlen(str);
-
-	HID_REPORT send_report;
-	send_report.length = length;
-	memcpy(send_report.data, str, length);
-
-	// ZTODO consider if it should be blocking or non-blocking
-	sendNB(&send_report);
-	return length;
+    int i = 0;
+    while (*str)
+    {
+        ensure_tx_space(1);
+        txbuf.queue(*str);
+        if ((txbuf.available() % 64) == 0)
+            usb->endpointSetInterrupt(HID_endpoint_in.bEndpointAddress, true);
+        i++;
+        str++;
+    }
+    usb->endpointSetInterrupt(HID_endpoint_in.bEndpointAddress, true);
+    return i;
 }
 
 
-bool USBMessageStream::USBEvent_EPIn(uint8_t, uint8_t) {
-	THEKERNEL->streams->printf("EPIn called!\n");
-	return false;}
-bool USBMessageStream::USBEvent_EPOut(uint8_t, uint8_t) {
-	THEKERNEL->streams->printf("EPOut called!\n");
-	// execute all pending messages
-	HID_REPORT recv_report;
-	while (readNB(&recv_report)) 
-	{
-		THEKERNEL->streams->printf("I have recieved an HID report!\n");
+void USBMessageStream::ensure_tx_space(int space)
+{
+    while (txbuf.free() < space)
+    {
+        usb->endpointSetInterrupt(HID_endpoint_in.bEndpointAddress, true);
+        usb->usbisr();
+    }
+}
 
-        struct SerialMessage message;
-        // ZTODO the following conversion is dangerous - unsigned to signed
-        message.message = string((const char *)recv_report.data);
-        if (message.message.find("sendme") != string::npos)
-        	puts("something");
-        message.stream = this;
-        THEKERNEL->streams->printf("message: %s", message.message.c_str());
-        THEKERNEL->call_event(ON_CONSOLE_LINE_RECEIVED, &message);
-	}
-	return false;}
+uint16_t USBMessageStream::writeBlock(const uint8_t * buf, uint16_t size)
+{
+    if (size > txbuf.free())
+    {
+        size = txbuf.free();
+    }
+    if (size > 0)
+    {
+        for (uint8_t i = 0; i < size; i++)
+        {
+            txbuf.queue(buf[i]);
+        }
+        usb->endpointSetInterrupt(HID_endpoint_in.bEndpointAddress, true);
+    }
+    return size;
+}
+
+
+bool USBMessageStream::USBEvent_EPIn(uint8_t bEP, uint8_t bEPStatus) 
+{
+    /*
+     * Called in ISR context
+     */
+
+    bool r = true;
+
+    if (bEP != HID_endpoint_in.bEndpointAddress)
+        return false;
+
+    iprintf("USBMessageStream:EpIn: 0x%02X\n", bEPStatus);
+
+   	HID_REPORT report;    
+
+    int l = txbuf.available();
+    iprintf("%d bytes queued\n", l);
+    if (l > 0)
+    {
+        if (l > MAX_PACKET_SIZE_EPBULK)
+            l = MAX_PACKET_SIZE_EPBULK;
+        iprintf("Sending %d bytes:\n\t", l);
+        int i;
+        for (i = 0; i < l; i++) {
+            txbuf.dequeue(&report.data[i]);
+            if (report.data[i] >= 32 && report.data[i] < 128)
+                iprintf("%c", report.data[i]);
+            else {
+                iprintf("\\x%02X", report.data[i]);
+            }
+        }
+        iprintf("\nSending...\n");
+        report.length = l;
+        send(&report);
+        iprintf("Sent\n");
+        if (txbuf.available() == 0)
+            r = false;
+    }
+    else
+    {
+        r = false;
+    }
+    iprintf("USBMessageStream:EpIn Complete\n");
+    return r;
+}
+
+bool USBMessageStream::USBEvent_EPOut(uint8_t bEP, uint8_t bEPStatus) 
+{
+    /*
+     * Called in ISR context
+     */
+
+    bool r = true;
+
+    iprintf("USBMessageStream:EpOut\n");
+    if (bEP != HID_endpoint_out.bEndpointAddress)
+        return false;
+
+	if (rxbuf.free() < MAX_PACKET_SIZE_EPBULK)
+		return false;
+
+
+   	HID_REPORT report;
+
+    // we read the report, and put the data in the circular buffer
+    read(&report);
+    iprintf("Read report of length %lu\n", report.length);
+	for (uint8_t i = 0; i < report.length; i++) {
+
+        if (flush_to_nl == false)
+            rxbuf.queue(report.data[i]);
+
+        if (report.data[i] >= 32 && report.data[i] < 128)
+        {
+            iprintf("%c", report.data[i]);
+        }
+        else
+        {
+            iprintf("\\x%02X", report.data[i]);
+        }
+
+        if (report.data[i] == '\n' || report.data[i] == '\r')
+        {
+            if (flush_to_nl)
+                flush_to_nl = false;
+            else
+                nl_in_rx++;
+        }
+        else if (rxbuf.isFull() && (nl_in_rx == 0))
+        {
+            // to avoid a deadlock with very long lines, we must dump the buffer
+            // and continue flushing to the next newline
+            rxbuf.flush();
+            flush_to_nl = true;
+        }        
+	}   
+
+    iprintf("\nQueued, %d empty\n", rxbuf.free());
+
+    if (rxbuf.free() < MAX_PACKET_SIZE_EPBULK)
+    {
+        // if buffer is full, stall endpoint, do not accept more data
+        r = false;
+
+        if (nl_in_rx == 0)
+        {
+            // we have to check for long line deadlock here too
+            flush_to_nl = true;
+            rxbuf.flush();
+
+            // and since our buffer is empty, we can accept more data
+            r = true;
+        }
+    }
+
+	usb->readStart(HID_endpoint_out.bEndpointAddress, MAX_PACKET_SIZE_EPBULK);
+    iprintf("USBMessageStream:EpOut Complete\n");
+    return r;
+}
--- src/libs/USBDevice/USBHID/USBMessageStream.h
+++ src/libs/USBDevice/USBHID/USBMessageStream.h
@@ -6,6 +6,7 @@
 #define USBMESSAGESTREAM_H
 
 #include "USBHID.h"
+#include "CircBuffer.h"
 
 #include "Module.h"
 #include "StreamOutput.h"
@@ -17,13 +18,25 @@ public:
 
     int puts(const char *);
 	
+    uint8_t available();
+
+    CircBuffer<uint8_t> rxbuf;
+    CircBuffer<uint8_t> txbuf;
+
     void on_module_loaded(void);
     void on_main_loop(void *);
+
+
+    uint16_t writeBlock(const uint8_t * buf, uint16_t size);
 
 protected:
     bool USBEvent_EPIn(uint8_t, uint8_t);
     bool USBEvent_EPOut(uint8_t, uint8_t);
+
+    void ensure_tx_space(int);
 	
+    volatile int nl_in_rx;
+    bool flush_to_nl;
 };
 
 #endif
