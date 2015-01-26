67839e15225a8282721f521075a2bd76bea03e18
malpa <malpa@gmail>
2015-01-26 17:22:02 +0100

    Added missing file from previous commit.

--- src/modules/tools/endstops/EndstopsPublicAccess.h
+++ src/modules/tools/endstops/EndstopsPublicAccess.h
@@ -5,5 +5,5 @@
 #define endstops_checksum    CHECKSUM("endstop")
 #define trim_checksum        CHECKSUM("trim")
 #define home_offset_checksum CHECKSUM("home_offset")
-
+#define homed_axes_checksum  CHECKSUM("homed_axes")
 #endif
