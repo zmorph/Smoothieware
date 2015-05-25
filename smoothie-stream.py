68561f752420507891b7f9dde7cc1a9ab3fe9129
Maciej Dębski <md319428@students.mimuw.edu.pl>
2015-05-25 14:58:47 +0200

    Measure and display streaming time.

--- smoothie-stream.py
+++ smoothie-stream.py
@@ -9,6 +9,7 @@ from __future__ import print_function
 import sys
 import telnetlib
 import argparse
+import datetime
 
 # Define command line argument interface
 parser = argparse.ArgumentParser(description='Stream g-code file to Smoothie over telnet.')
@@ -22,6 +23,8 @@ args = parser.parse_args()
 
 f = args.gcode_file
 verbose = not args.quiet
+
+starttime = datetime.datetime.now()
 
 # Stream g-code to Smoothie
 print("Streaming " + args.gcode_file.name + " to " + args.ipaddr)
@@ -51,7 +54,9 @@ while okcnt < linecnt:
 tn.write("exit\n")
 tn.read_all()
 
-print("Done")
+endtime = datetime.datetime.now()
+delta = endtime - starttime
+# Strip microseconds.
+delta -= datetime.timedelta(microseconds=delta.microseconds)
 
-
-
+print("Done, took %s." % str(delta))
